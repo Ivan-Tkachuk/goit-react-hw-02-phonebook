a013e70063ff5d79c50593f16497421c45060ee5
Ivan-Tkachuk <tkachuchella@gmail.com>
2023-02-25 19:48:54 +0200

    goit-react-hw-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework-2 phonebook
-    </div>
-  );
-};
+import React from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+import { Container } from './App.styled';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.state.contacts.filter(contact => contact.name === name).length
+      ? alert(`${newContact.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [newContact, ...prevState.contacts],
+        }));
+    console.log(this.state.contacts);
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+  list-style: disc;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-left: 20px;
+
+  font-size: 12px;
+  font-weight: 600;
+
+  width: 60px;
+
+  &:active {
+    background-color: blue;
+    border: none;
+  }
+`;
+
+export const Text = styled.p`
+  display: inline-block;
+  margin: 0;
+`;
